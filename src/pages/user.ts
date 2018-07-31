3e9b6ca8e49e78e6fbbe0c3d40232b464d9de62e
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2018-07-31 20:02:37 +0300

    Update ping.

--- src/pages/user.ts
+++ src/pages/user.ts
@@ -7,14 +7,15 @@ import {LessThan, MoreThan, Not} from "typeorm";
 
 const handler = async (req: Request, res: Response) => {
     let session: Session = req['session'],
-        ping = session.getCache('ping');
-    if (!ping) {
+        ping = session.getCache('ping', void 0);
+    if (ping === void 0) {
         try {
-            let user = await session.user(), abilities: { [key: string]: any } = [], phone, leftSubscribe = null;
+            const user = await session.user();
+            ping = null;
             if (user) {
                 const date = new Date;
                 const month = date.getFullYear() * 12 + date.getMonth();
-                abilities = (await Ability.createQueryBuilder('ability')
+                const abilities: { [key: string]: any } = (await Ability.createQueryBuilder('ability')
                     .innerJoin('ability.type', 'type', 'type.name IS NOT NULL')
                     .addSelect('type')
                     .addSelect("SUM(ability.count)", 'ability_count')
@@ -31,7 +32,7 @@ const handler = async (req: Request, res: Response) => {
                         return result;
                     }, {});
 
-                phone = await user.getPhone();
+                const phone = await user.getPhone();
                 abilities.mailings = user.mailings.map((m: Mailing) => m.id);
 
                 let item = await Item.createQueryBuilder('item')
@@ -43,25 +44,25 @@ const handler = async (req: Request, res: Response) => {
                     .andWhere('user.id = :user', {user: user.id})
                     .addOrderBy('end', 'DESC')
                     .getOne();
-
+                let leftSubscribe = null;
                 if (item) {
-                    let date: Date = new Date();
+                    const date: Date = new Date();
                     date.setHours(0, 0, 0);
-                    let dateSubscribe = new Date(Math.floor(item.end / 12), item.end % 12 + 1, 0, 0, 0, 0);
+                    const dateSubscribe = new Date(Math.floor(item.end / 12), item.end % 12 + 1, 0, 0, 0, 0);
                     leftSubscribe = Math.floor((dateSubscribe.getTime() - date.getTime()) / 86400000) + 1;
                 }
+                ping = {
+                    id: user ? user.id : 0,
+                    email: user ? user.email : '',
+                    name: user ? user.name : '',
+                    abilities,
+                    sname: cookie,
+                    sid: session.getId(),
+                    phone: phone ? phone.phone : '',
+                    leftSubscribe
+                };
             }
 
-            ping = {
-                id: user ? user.id : 0,
-                email: user ? user.email : '',
-                name: user ? user.name : '',
-                abilities,
-                sname: cookie,
-                sid: session.getId(),
-                phone: phone ? phone.phone : '',
-                leftSubscribe
-            };
 
             session.setCache('ping', ping, 300);
         } catch (e) {
